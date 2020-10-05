61438c509bf38c23ca8c874dfb5dbdf80bcf5472
deviprajwala <deviprajwala3@gmail.com>
2020-10-05 20:48:44 +0530

    done!!!

--- cfcss.cpp
+++ cfcss.cpp
@@ -4,8 +4,8 @@
 #include<cstdlib>
 using namespace std;
 
-
-//program to find si,di,D of a control flow graph
+//to generate the accurate value of signatures for each of the nodes so as to check the control flow of the graphs
+//program to find si,di,D for nodes of a control flow graph
 
 struct node
 {
@@ -15,8 +15,11 @@ struct node
   int d;
   int D;
   int links;
+  //number of child nodes
   struct node *link[10];
+  //link to the child nodes
   struct node *parent;
+  //link to the parent node
 };
 //node of the control flow graph which contains information required to check signature
 
@@ -28,6 +31,23 @@ map<int,int>mp;
 
 vector<pair<int,int> >diamond;
 //to track nodes in which D should be changed
+
+
+struct node* getnode(int num,struct node* cur);
+//function to get a new node whose information is set as per the requirement
+void print(vector<vector<int> >matrix);
+//function to print the elements of the matrix
+int count(int num,vector<vector<int> > matrix,struct node* cur);
+//to count the number of child nodes and if there is branch fan in node then it is stored as pair in vector
+void generate_graph(vector<vector<int> >matrix);
+//to generates the control flow graph
+void diagraph(struct node *root);
+//to print the commands to a text file to generate a graph 
+struct node* update_d(int val1,struct node* root);
+//to update the value of D wherever necesssary 
+struct node* compute_signature(struct node *root);
+//to compute the values of s and d for each of the node 
+
 
 struct node* getnode(int num,struct node* cur)
 //function to get a new node whose information is set as per the requirement
@@ -52,6 +72,7 @@ void print(vector<vector<int> >matrix)
   }
 }
 int count(int num,vector<vector<int> > matrix,struct node* cur)
+//to count the number of child nodes and if there is branch fan in node then it is stored as pair in vector
 {
    int cnt=0;
    for(int i=1;i<matrix[0].size();i++)
@@ -59,13 +80,18 @@ int count(int num,vector<vector<int> > matrix,struct node* cur)
       if(matrix[num][i]==1)
       {
         struct node* x=getnode(i,cur);
+        //if there is a connection between node parent and its value is updated
         if(mp.find(i)!=mp.end())
+        //if two nodes have same child, it is noted in the map named diamond
         {
           diamond.push_back(make_pair(mp[i],cur->val));
         }
         mp[i]=cur->val;
+        //map value is added
         cur->link[cnt]=x;
+        //link of the node is set to its child
         cnt++;
+        //value of cnt is incremented
       }
    }
    return cnt;
@@ -77,23 +103,29 @@ void generate_graph(vector<vector<int> >matrix)
   {
    struct node* cur=s.top();
    int idx=0,link_count=cur->links;
+   //number of child nodes is stored in a variable
    s.pop();
    while(link_count!=0)
    {
-
      struct node* x=cur->link[idx];
-     
-
      s.push(x);
+     //child nodes are pushed into the stack
+
      x->d=x->val;
      x->links=count(x->val,matrix,x);
+     //child nodes of x is calculated and it is updated with its parent
+
      link_count-=1;
+     //link count is decremented by one
+
      idx++;
+     //index value is incremented by one
    }
   }
   return;
 }
 void diagraph(struct node *root)
+//to print the commands to a text file to generate a graph 
 {
   stack <struct node* >s;
   s.push(root);
@@ -103,35 +135,47 @@ void diagraph(struct node *root)
   {
    struct node* x=s.top();
    cout<<x->val<<"[ shape = record label=\""<<x->val<<"|s="<<x->s<<"|d="<<x->d<<"|g="<<x->g;
+  //to print the record values of a node
    cout<<"|D="<<x->D<<"\"]\n";
    s.pop();
    int id=0;
    while(x->links!=0)
    {
      s.push(x->link[id]);
+     //child nodes are pushed into stack
      if(check.find(x->val)==check.end()||check[x->val]!=x->link[id]->val)
+     //to avoid the multiple copies of same node relations with the help of map
      {
        check[x->val]=x->link[id]->val;
+       //values are updated in the map
        cout<<x->val<<"->"<<x->link[id]->val<<"\n";
      }
      x->links-=1;
+     //link value decremented by one
      id++;
+     //id value incremented by one
+
    }
   }  
   cout<<"}";
 }
+
 struct node* update_d(int val1,struct node* root)
+//to update the value of D wherever necesssary,while having one node present in diamond pattern 
 {
   int value;
   for(int i=0;i<diamond.size();i++)
   {
-    if(diamond[i].second==val1)
+    if(diamond[i].second==val1)   
     {
       value=diamond[i].first;
+      //value of the other node is found
     }
   }
  //to find the node with value val
+
  s.push(root);
+ //seach the node whose val is value
  while(!s.empty())
   {
    struct node* x=s.top();
@@ -149,23 +193,31 @@ struct node* update_d(int val1,struct node* root)
      id++;
    }
   } 
+  // the node with value is not found then null is returned
   return NULL; 
 }
+
 struct node* compute_signature(struct node *root)
+//to compute the values of s and d for each of the node 
 {
   stack<struct node*>s;
   vector<int>v(100);
   s.push(root);
   while(!s.empty())
+  //depth first traversal is used to visit each of the node and calculate the signature
   {
     struct node* x=s.top();
     int lcount=x->links;
     v.push_back(x->val);
+    //visited node is noted
+
     s.pop();
     if(x->s==0)
     {
       x->g=x->parent->g^x->d;
+      //gi=g(prev)^di
       x->s=x->g;
+      //si=gi
     }
     int idx=lcount-1;
     while(lcount!=0)
@@ -174,10 +226,15 @@ struct node* compute_signature(struct node *root)
       {
         //if a visited node is pushed to stack again then there must be a cycle
         struct node*y=update_d(x->val,root);
+        //node is obtained in which D has to be updated
         if(y!=NULL)
         {
          y->D=0;
+         //y D=0000;
+
          x->D=x->s^y->s;
+         //x D=s(prev1)^s(prev2)
+
          x->link[idx]->g=x->link[idx]->g^y->D;
         }
       }
@@ -203,9 +260,16 @@ int main()
   {
     int node1,node2;
     cin>>node1>>node2;
+    //nodes which are linked are read
+
     if(node1==node2)continue;
+    //if self loop is found 
+ 
     if(node1>node2) continue;
+    //if back edges are found
+
     matrix[node1][node2]=1;
+    //matrix is generated based on connected components
   }
  // print(matrix);
  //root node is set with the initial values required for the computation of conrol flow graph
@@ -220,12 +284,13 @@ int main()
  
   s.push(root);
   generate_graph(matrix);
-/*  cout<<root->val<<"\n";
-  cout<<root->link[0]->val<<" "<<root->link[1]->val<<"\n";
-  cout<<root->link[0]->link[0]->val<<" "<<root->link[0]->link[1]->val<<root->link[1]->link[0]->val<<"\n";
-  cout<<root->link[0]->link[0]->link[0]->val;*/
+  //function is called to generate a graph
+
   root=compute_signature(root);
+  //signature for each of the node is calculated
+
   diagraph(root); 
- 
+  //function is called to print the commands needed to generate graph
+
   return 0;
 }
